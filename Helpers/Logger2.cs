6c3771128fb851c2fbba680526f071b1ac1c186e
Jorge Eduardo Hernandez Vargas <jorgeeduardohv@hotmail.com>
2018-11-18 22:12:29 -0600

    Helpers for Logs added

---
+++ Helpers/Logger2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class Logger2
+    {
+        private static Logger2 instance = new Logger2();
+        private const string logFilePath = @"D:\NodosBlockChain\Node2\Log.txt";
+
+        private Logger2()
+        {
+
+        }
+
+        public static Logger2 getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger2();
+            }
+            return instance;
+        }
+
+        public void Debug(string message)
+        {
+            TextWriter tsw = new StreamWriter(logFilePath, true);
+            tsw.Write(String.Format("{0} - {1}", DateTime.Now, message));
+            tsw.WriteLine();
+            tsw.Close();
+        }
+
+        public void CleanLog()
+        {
+            File.WriteAllText(logFilePath, String.Empty);
+        }
+    }
+}
---
+++ Helpers/Logger3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class Logger3
+    {
+        private static Logger3 instance = new Logger3();
+        private const string logFilePath = @"D:\NodosBlockChain\Node3\Log.txt";
+
+        private Logger3()
+        {
+
+        }
+
+        public static Logger3 getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger3();
+            }
+            return instance;
+        }
+
+        public void Debug(string message)
+        {
+            getInstance();
+            TextWriter tsw = new StreamWriter(logFilePath, true);
+            tsw.Write(String.Format("{0} - {1}", DateTime.Now, message));
+            tsw.WriteLine();
+            tsw.Close();
+        }
+
+        public void CleanLog()
+        {
+            File.WriteAllText(logFilePath, String.Empty);
+        }
+    }
+}
